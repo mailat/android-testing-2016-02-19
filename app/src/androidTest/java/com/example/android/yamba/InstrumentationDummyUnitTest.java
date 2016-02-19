2c7b56411eb47a6b3e92822cc5b0569f96ca7ea3
Marius Mailat <marius.mailat@appsrise.com>
2016-02-19 14:58:03 +0200

    Dummy classes.

---
+++ app/src/androidTest/java/com/example/android/yamba/InstrumentationDummyUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.android.yamba;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class InstrumentationDummyUnitTest {
+
+    @Test
+    public void dummyUnitTest() {
+        // do nothing but compile, run, and pass
+    }
+}
---
+++ app/src/test/java/com/example/android/yamba/JVMDummyUnitTest.java
@@ -0,0 +1,11 @@
+package com.example.android.yamba;
+
+import org.junit.Test;
+
+public class JVMDummyUnitTest {
+
+    @Test
+    public void dummyUnitTest() {
+        // do nothing but compile, run, and pass
+    }
+}
